49712a39047b2cca0181a395023ecf2c9d88bc2b
unknown <akashranga27@gmail.com>
2023-08-25 12:38:02 +0530

    new project

---
+++ index.js
@@ -0,0 +1,33 @@
+
+//Basics js
+//alert('welcome javascript');
+// alert(5+4);
+console.log("hi javascript welcome");
+
+const name = "VIGNESH";
+const dob = "27-03-2000";
+let msg ="hello Start to learn " +name+" my dob "+dob+"";
+
+// console.log(msg);
+
+// JavaScript Primitive Datatype
+
+// let firstName ="Vignesh"; // String
+// let age = 23;  // integer / number
+// let young = true; // bollean
+// let lastName = undefined;  // undefined
+// let trophy = null;  // null
+
+// Object TOPIC
+
+let person = {
+    name:"sai",
+    age: 23,
+    isAlive: true,
+    gender:"female",
+    address:"chennai"
+}
+//dot notation
+console.log(person.isAlive)
+// bracket notation
+console.log(person['gender'])
